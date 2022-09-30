2e1af5850588f818f01141f83fac052b30a83c74
anothergravity <yoonho8964@naver.com>
2022-09-30 11:25:23 +0900

    복구작업

---
+++ 오전수업/JS_제어문/boom.js
@@ -0,0 +1,40 @@
+// boom.js
+
+var board = new Array(400);
+board.fill(0); //배열에 채우기
+
+for (var i = 0; i < 200; ) {
+  let pos = Math.floor(Math.random() * 400);
+  if (board[pos] != 1) {
+    board[pos] = 1;
+    i++;
+  }
+}
+
+window.onload = function () {
+  var table = '<table>';
+  for (var i = 0; i < 20; i++) {
+    table += '<tr>';
+    for (var k = 0; k < 20; k++) {
+        table += '<td class="bm" data-idx='+(i*20+k)+'></td>';
+    }
+    table += '</tr > ';
+  }
+  document.getElementById('wrap').innerHTML = table;
+
+  var bm = document.getElementsByClassName('bm');
+  
+  //alert(bm.length);
+  //bm[0].style.background ='red';
+  for(var i in bm){
+    bm[i].addEventListener('click',function(){
+      let n = this.dataset.idx;
+      if(board[n]==1){
+        this.style.background=
+        'url(../../image/0919/bomb.jpg) no-repeat center center';
+        this.style.backgroundSize = 'cover';
+        //this.innerHTML="<img src='../../image/0919/bomb.jpg'>";
+      } else this.style.background = 'rgb(230,230,230)';
+    });
+  }
+};
